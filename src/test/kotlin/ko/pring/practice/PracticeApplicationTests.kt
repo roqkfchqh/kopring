1f5cad466259dd2a5593e908d787e68f88218180
lcy <plmokn48@daum.net>
2025-04-15 12:49:18 +0900

    first commit

---
+++ src/test/kotlin/ko/pring/practice/PracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package ko.pring.practice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
