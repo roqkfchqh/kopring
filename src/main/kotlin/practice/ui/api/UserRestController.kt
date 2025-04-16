cb33463dc925de16fff9b11ed13a9e1fdc06f3fd
lcy <plmokn48@daum.net>
2025-04-16 21:11:07 +0900

    feat: 회원가입 api, 기본 ApiResponse반환형

---
+++ src/main/kotlin/practice/ui/api/UserRestController.kt
@@ -0,0 +1,22 @@
+package practice.ui.api
+
+import jakarta.validation.Valid
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import practice.application.RegisterUserRequest
+import practice.application.UserAuthenticationService
+
+@RequestMapping("/api/users")
+@RestController
+class UserRestController(
+    private val userAuthenticationService: UserAuthenticationService
+) {
+    @PostMapping
+    fun generateToken(@RequestBody @Valid request: RegisterUserRequest): ResponseEntity<ApiResponse<String>> {
+        val token = userAuthenticationService.generateTokenByRegister(request)
+        return ResponseEntity.ok(ApiResponse.success(token))
+    }
+}
